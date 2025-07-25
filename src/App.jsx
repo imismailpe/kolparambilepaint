c98ac6c04fd610f6e122ca900a9abb6b72e2b876
Ismail PE <mailismailpe@gmail.com>
2025-07-25 14:26:42 +0530

    repaint working

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [selectedColor, setSelectedColor] = useState("#d4d4d4");
+  const [appliedColor, setAppliedColor] = useState("#d4d4d4");
+  const [loading, setLoading] = useState(true);
+  const applyPaint = () => {
+    setLoading(true);
+    setAppliedColor(selectedColor);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+  return (
+    <>
+      <h1>Kolparambil</h1>
+      <p className="read-the-docs">Choose a color and apply</p>
+      <div className="card">
+        <input
+          type="color"
+          value={selectedColor}
+          onChange={(e) => setSelectedColor(e.target.value)}
+          className="color-picker"
+        />
+
+        <button onClick={applyPaint} className="apply-button">
+          Apply Paint
+        </button>
+
+        <div
+          className="image-wrapper"
+          style={{ borderColor: appliedColor, border: "1px solid" }}
+        >
+          {loading ? (
+            "Painting.."
+          ) : (
+            <img
+              src={`https://res.cloudinary.com/reviewdepo/image/upload/e_gen_recolor:prompt_walls;to-color_${appliedColor.replace(
+                "#",
+                ""
+              )};multiple_true/sittingrom_fsnbht`}
+              alt="Sample"
+              width={320}
+              onLoad={() => setLoading(false)}
+            />
+          )}
+        </div>
+
+        <p className="color-code">Selected Color code: {appliedColor}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
